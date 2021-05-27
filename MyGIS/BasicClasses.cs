f8530ab87a281489d6d1e30596e325da671326eb
Iluvenis <Iluvenis@outlook.com>
2021-05-27 14:36:58 +0800

    0.0.1

--- MyGIS/BasicClasses.cs
+++ MyGIS/BasicClasses.cs
@@ -8,6 +8,57 @@ namespace MyGIS
 {
     class BasicClasses
     {
-        
+        class GISVertex
+        {
+            public double x, y;
+
+            public GISVertex(double _x, double _y)
+            {
+                x = _x;
+                y = _y;
+            }
+
+            public double Distance(GISVertex vertex)
+            {
+                return Math.Sqrt(Math.Pow(vertex.x - x, 2) + Math.Pow(vertex.y - y, 2));
+            }
+        }
+
+        class GISPoint
+        {
+            public GISVertex Location;
+            public string Attribute;
+
+            public GISPoint(GISVertex vertex, string attribute)
+            {
+                Location = vertex;
+                Attribute = attribute;
+            }
+
+            public void DrawPoint(Graphics graphics)
+            {
+                graphics.FillEllipse(new SolidBrush(Color.Red), new Rectangle((int)(location.x) - 3, (int)(location.y) - 3, 6, 6));
+            }
+
+            public void DrawAttribute(Graphics graphics)
+            {
+                graphics.DrawString(Attribute, new Font("黑体", 20), new SolidBrush(Color.Green), new PointF((int)(Location.x), (int)(location.y)));
+            }
+
+            public double Distance(GISVertex vertex)
+            {
+                return Location.Distance(vertex);
+            }
+        }
+
+        class GISLine
+        {
+            List<GISVertex> AllVertices;
+        }
+
+        class GISPolygon
+        {
+            List<GISVertex> AllVertices;
+        }
     }
 }
